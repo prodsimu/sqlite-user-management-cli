a2077d65df55244a3844f2f0fcc7bbb4a818a031
simu-T14 <139299982+prodsimu@users.noreply.github.com>
2026-03-02 09:09:58 -0300

    feat: add admin_menu to Menu class

--- app/ui/menus.py
+++ app/ui/menus.py
@@ -4,3 +4,12 @@ class Menu:
     def public_menu() -> None:
         print("1 - Login")
         print("0 - Shutdown")
+
+    @staticmethod
+    def admin_menu() -> None:
+        print("=== ADMIN MENU ===")
+        print("1 - Create user")
+        print("2 - List users")
+        print("3 - Update user")
+        print("4 - Delete user")
+        print("0 - Logout")
