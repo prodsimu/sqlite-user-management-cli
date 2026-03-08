ffd3af1376e2aa00e2459557c85cf9dd96aaeef3
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-08 19:10:30 -0300

    test: add UserRepository list_all_users test

--- tests/repositories/test_user_repository.py
+++ tests/repositories/test_user_repository.py
@@ -18,3 +18,15 @@ def test_user_repository_find_by_username(user_repository):
     user = user_repository.find_by_username("ignatius123")
 
     assert user.name == "ignatius"
+
+
+def test_user_repository_list_all_users(user_repository):
+    user_repository.create("ignatius", "ignatius123", "hashed_password", "user")
+    user_repository.create("joseph", "joseph123", "hashed_password", "user")
+    user_repository.create("peter", "peter123", "hashed_password", "user")
+
+    users_list = user_repository.list_all_users()
+
+    assert users_list[0].name == "ignatius"
+    assert users_list[1].name == "joseph"
+    assert users_list[2].name == "peter"
